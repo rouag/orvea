83c6c7cafac90090079a3e1a8040737cf0a3614d
karim <karimrouag86@gmail.com>
2017-01-06 18:06:38 +0100

    [ADD] view hr_hr_contract and raport and decision type

--- smart_hr/models/hr_decision.py
+++ smart_hr/models/hr_decision.py
@@ -12,14 +12,24 @@ class HrDecision(models.Model):
     name = fields.Char(string='المسمى', required=1)
     decision_type_id = fields.Many2one('hr.decision.type', string='نوع القرار', required=1)
     date = fields.Date(string='التاريخ', required=1)
+    employee_id=fields.Many2one('hr.employee',string='الموظف',required=1,)
     text = fields.Html(string='نص القرار')
 
     @api.onchange('decision_type_id')
     def onchange_decision_type_id(self):
         if self.decision_type_id:
-            self.text = self.decision_type_id.text
-
-
+            decision_type_line = self.env['hr.decision.type'].search([('id', '=', self.decision_type_id.id)
+                                                ])
+           # employee=self.employee_id.id
+           # employe=str(employee)
+          #  print"employe",employee
+            if decision_type_line.text:
+                rel_text=decision_type_line.text
+                rep_text=rel_text.replace('EMPLOYE','123456' )
+                
+                self.text = rep_text
+                print"text",self.text
+                
 class HrDecisionType(models.Model):
     _name = 'hr.decision.type'
     _description = u'نوع القرار'
