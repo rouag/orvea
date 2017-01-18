bad0abb719c7eb2a815bcb771e03927af31f7f9c
abderrahmen khalledi <abderrahmen.khalledi@smart-etech.com>
2017-01-18 18:02:09 +0100

    [IMP] نموذج طلب نقل وظيفة

--- smart_hr/job/parser/report_parser.py
+++ smart_hr/job/parser/report_parser.py
@@ -155,3 +155,69 @@ class ReportJobDescription(osv.AbstractModel):
     _inherit = 'report.abstract_report'
     _template = 'smart_hr.report_job_description'
     _wrapped_report_class = JobDescriptionReport
+
+
+class JobMoveDepReport(report_sxw.rml_parse):
+
+    def __init__(self, cr, uid, name, context):
+        super(JobMoveDepReport, self).__init__(cr, uid, name, context=context)
+        self.localcontext.update({
+            'get_lines': self._get_lines,
+            'format_time': self._format_time,
+            'get_current_date': self._get_current_date,
+            'get_job': self._get_job,
+            'get_job_create_date': self._get_job_create_date,
+            'get_employee_job_decision': self._get_employee_job_decision,
+            'get_employee_job_decision_history': self._get_employee_job_decision_history,
+            'get_employee_last_training': self._get_employee_last_training
+        })
+
+    def _get_employee_last_training(self, job):
+        hr_candidates_ids = self.pool.get('hr.candidates').search(self.cr, self.uid, [('training_id.state', '=', 'done'), ('employee_id', '=', job.employee.id)])
+        if hr_candidates_ids:
+            last_hr_candidate_id = hr_candidates_ids[len(hr_candidates_ids) - 1]
+            if last_hr_candidate_id:
+                hr_candidate = self.pool.get('hr.candidates').browse(self.cr, self.uid, [last_hr_candidate_id])
+            return hr_candidate.training_id
+        return False
+
+    def _get_employee_job_decision_history(self, job):
+        hr_decision_appoint_ids = self.pool.get('hr.decision.appoint').search(self.cr, self.uid, [('employee_id', '=', job.employee.id), ('state', '=', 'done'), ('active', '=', False)])
+        if hr_decision_appoint_ids:
+            hr_decision_appoints = self.pool.get('hr.decision.appoint').browse(self.cr, self.uid, hr_decision_appoint_ids)
+            return hr_decision_appoints
+        return False
+
+    def _get_employee_job_decision(self, job):
+        hr_decision_appoint_ids = self.pool.get('hr.decision.appoint').search(self.cr, self.uid, [('job_id', '=', job.id), ('state', '=', 'done')])
+        if hr_decision_appoint_ids:
+            last_decision_appoint_id = hr_decision_appoint_ids[len(hr_decision_appoint_ids) - 1]
+            if last_decision_appoint_id:
+                hr_decision_appoint = self.pool.get('hr.decision.appoint').browse(self.cr, self.uid, [last_decision_appoint_id])[0]
+                return hr_decision_appoint
+        return False
+
+    def _get_job_create_date(self, job):
+        return fields.Datetime.from_string(job.create_date).strftime("%Y-%m-%d")
+
+    def _get_job(self, data):
+        job_id = data['job_id'][0]
+        return self.pool.get('hr.job').browse(self.cr, self.uid, [job_id])[0]
+
+    def _get_current_date(self):
+        now = datetime.datetime.now()
+        return now.strftime("%Y-%m-%d")
+
+    def _format_time(self, time_float):
+        hour, minn = float_time_convert(time_float)
+        return '%s:%s' % (str(hour).zfill(2), str(minn).zfill(2))
+
+    def _get_lines(self, data):
+        return []
+
+
+class ReportJobMoveDep(osv.AbstractModel):
+    _name = 'report.smart_hr.report_job_move_dep'
+    _inherit = 'report.abstract_report'
+    _template = 'smart_hr.report_job_move_dep'
+    _wrapped_report_class = JobMoveDepReport
