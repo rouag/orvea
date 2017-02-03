b6a3f4acd53112e0f142a87f22958ae69e656f78
samar <samar.sliti@smart-etech.com>
2017-02-03 17:54:23 +0100

    report حصر الاجازات

--- smart_hr/holidays/parser/resume_holidays_report.py
+++ smart_hr/holidays/parser/resume_holidays_report.py
@@ -11,29 +11,27 @@ class ResumeHoldaysReport(report_sxw.rml_parse):
         super(ResumeHoldaysReport, self).__init__(cr, uid, name, context=context)
         self.localcontext.update({
             'get_lines': self._get_lines,
-            'format_time': self._format_time,
-            'get_current_date': self._get_current_date,
+            'get_employee': self._get_employee,
         })
-
-    def _get_current_date(self):
-        now = datetime.datetime.now()
-        return now.strftime("%Y-%m-%d")
-
-    def _format_time(self, time_float):
-        hour, minn = float_time_convert(time_float)
-        return '%s:%s' % (str(hour).zfill(2), str(minn).zfill(2))
 
     def _get_lines(self, data):
         date_from = data['date_from']
         date_to = data['date_to']
         employee_id = data['employee_id'] and data['employee_id'][0] or False
-        domain = [('date', '>=', date_from), ('date', '<=', date_to), ('state', '=', 'done')]
+        domain = [('date_from', '<=', date_to), ('date_to', '>=', date_from)]
         if employee_id:
             domain.append(('employee_id', '=', employee_id))
-       
         holidays_obj = self.pool.get('hr.holidays')
         summary_ids = holidays_obj.search(self.cr, self.uid, domain)
-        return holidays_obj.browse(self.cr, self.uid, summary_ids)
+        if summary_ids:
+            return holidays_obj.browse(self.cr, self.uid, summary_ids)
+        else:
+            return []
+        
+    def _get_employee(self, data):
+        employee_id = data['employee_id'] and data['employee_id'][0] or False
+        emp_obj = self.pool.get('hr.employee').browse(self.cr, self.uid, employee_id)
+        return emp_obj
 
 
 class ReportResumeHolidays(osv.AbstractModel):
