eb8e457ad63725ff8b7eb156adfd4ad6016695ba
smart <smart@smart-HP-280-G1-MT>
2017-02-07 20:11:33 +0100

    [fix] sanction

--- smart_hr/models/hr.py
+++ smart_hr/models/hr.py
@@ -76,7 +76,7 @@ class HrEmployee(models.Model):
     passport_place = fields.Char(string=u'مكان إصدار جواز السفر')
     passport_end_date = fields.Date(string=u'تاريخ انتهاء جواز السفر ')
     display_name = fields.Char(compute='_compute_display_name', string='display Name', select=True)
-    sanction_ids = fields.One2many('hr.employee.sanction', 'employee_id', string=u'العقوبات')
+    sanction_ids = fields.One2many('hr.sanction.ligne', 'employee_id', string=u'العقوبات')
     bank_account_ids = fields.One2many('res.partner.bank', 'employee_id', string=u'الحسابات البنكِيّة')
     education_level_ids = fields.One2many('hr.employee.job.education.level', 'employee_id', string=u'المستوى التعليمي')
     education_level_id = fields.Many2one('hr.employee.education.level', string=u'المستوى التعليمي ')
