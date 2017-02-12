259fe0dc81dc26e5b0d8313c504f1aa09bbba44a
karim <karimrouag86@gmail.com>
2017-02-12 21:42:23 +0100

    [ADD] madate task

--- smart_hr/hr_time_out/hr_mandate_task.py
+++ smart_hr/hr_time_out/hr_mandate_task.py
@@ -33,17 +33,31 @@ class HrMandateTask(models.Model):
     note = fields.Text(string=u'الملاحظات', readonly=1, states={'draft': [('readonly', 0)]})
     date_mandate_out = fields.Date(string='تاريخ قرار ')
     file_mandate = fields.Binary(string='صورة قرار ')
+    transport_alocation = fields.Boolean(string='بدل نقل')
+    net_salary = fields.Boolean(string=' الراتب')
+    alowance_bonus = fields.Boolean(string=' البدلات و التعويضات و المكافات')
+    the_availability  = fields.Selection([
+        ('hosing_and_food', u'السكن و الطعام '),
+        ('hosing_or_food', u'السكن أو الطعام '),
+         ('nothing', u'لا شي '),
+         ], string=u'الجهة توفر', default='hosing_and_food', advanced_search=True) 
     mandate_type = fields.Selection([
         ('internal', u'داخلى'),
         ('external', u'خارجى'),
     ], string=u'نوع الأنتداب', default='internal', advanced_search=True)
     city_id = fields.Many2one('res.city', string=u'المدينة')
     mandate_category_id = fields.Many2one('hr.mandate.category', string=u'فئة التصنيف')
-    state = fields.Selection([('draft', '  طلب'),
-                             ('waiting', '  المعالي أو اللجنة'),
-                             ('extern', 'جهة خارجية'),
-                             ('done', 'اعتمدت'),
-                             ('cancel', 'ملغاة')], string='الحالة')
+    state = fields.Selection([
+                              ('draft', u'طلب'),
+                              ('audit', u'دراسة طلب'),
+                              ('waiting', u'اللجنة'),
+                              ('done', u'اعتمدت'),
+                              ('refuse', u'رفض'),
+                              ('order', u'دراسة التقرير'),
+                              ('finish', u'منتهية')
+                              ], string=u'حالة', default='draft', advanced_search=True)
+    
+  
     task_name = fields.Char(string=u'وصف المهمة')
     date_from = fields.Date(string=u'من')
     date_to = fields.Date(string=u'الى')
@@ -52,25 +66,32 @@ class HrMandateTask(models.Model):
     @api.multi
     def action_draft(self):
         for time_out in self:
-            time_out.state = 'waiting'
-            
+            time_out.state = 'audit'
+         
+    @api.multi
+    def action_audit(self):
+        for time_out in self:
+            time_out.state = 'waiting'   
     @api.multi
     def action_waiting(self):
         for time_out in self:
-            time_out.state = 'extern'
+            time_out.state = 'done'
+
+    @api.multi
+    def action_done(self):
+        for time_out in self:
+            time_out.state = 'order'
+    
+    @api.multi
+    def action_order(self):
+        for time_out in self:
+            time_out.state = 'finish'
+
 
     @api.multi
     def action_refuse(self):
         for time_out in self:
-            time_out.state = 'draft'
-    
-    @api.multi
-    def action_extern(self):
-        for time_out in self:
-            time_out.state = 'extern'
-
-
-  
+            time_out.state = 'refuse'
 
 
     @api.depends('date_from', 'date_to')
@@ -107,5 +128,3 @@ class HrMandateCategory(models.Model):
         ('c', u'ج'),
     ], string=u'الفئات', default='c')
     country_ids = fields.Many2many('res.country', 'mandate_country_rel', 'count_id', 'dep_id', string=u'البلاد')
-
-   
