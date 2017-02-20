500cf50cb39aa048c081c5e1b23e120689390921
abderrahmen khalledi <abderrahmen.khalledi@smart-etech.com>
2017-02-20 10:15:51 +0100

    [IMP] payslip report

--- smart_hr/payroll/parser/report_parser.py
+++ smart_hr/payroll/parser/report_parser.py
@@ -13,8 +13,16 @@ class MessierSalaires(report_sxw.rml_parse):
         super(MessierSalaires, self).__init__(cr, uid, name, context=context)
         self.localcontext.update({
             'get_hijri_date': self._get_hijri_date,
-           
+            'get_allowances': self._get_allowances,
         })
+
+    def _get_allowances(self, line_ids):
+        allowance_ids = []
+        for line in line_ids:
+            if line.category == 'allowance':
+                allowance_ids.append(line)
+        print allowance_ids
+        return allowance_ids
 
     def _get_hijri_date(self, date, separator):
         '''
@@ -27,11 +35,9 @@ class MessierSalaires(report_sxw.rml_parse):
             return str(int(hijri_date.year)) + separator + str(int(hijri_date.month)) + separator + str(int(hijri_date.day))
         return None
 
-   
 
 class ReportMessierSalaires(osv.AbstractModel):
     _name = 'report.smart_hr.report_hr_messier_salaries'
     _inherit = 'report.abstract_report'
     _template = 'smart_hr.report_hr_messier_salaries'
     _wrapped_report_class = MessierSalaires
-
