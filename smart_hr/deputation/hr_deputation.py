f3466d2a4dc19d7a835c59447f5030183be48252
samar <samar.sliti@smart-etech.com>
2017-02-20 18:35:00 +0100

    fix deputation

--- smart_hr/deputation/hr_deputation.py
+++ smart_hr/deputation/hr_deputation.py
@@ -261,45 +261,20 @@ class HrDeputation(models.Model):
             type = ' إنتداب'
             self.env['hr.employee.history'].sudo().add_action_line(self.employee_id, deputation.decision_number,  deputation.date_from, type)
             deputation.state = 'finish'
-
+            
     @api.multi
     def action_refuse(self):
         for deputation in self:
             deputation.state = 'refuse'
-
+            
     @api.one
     @api.depends('date_from', 'date_to')
     def _compute_duration(self):
         if self.date_from and self.date_to:
             date_from = fields.Date.from_string(self.date_from)
             date_to = fields.Date.from_string(self.date_to)
-            daygenerator = (date_from + timedelta(x + 1) for x in xrange((date_to - date_from).days))
-            sum_minus_weekends = sum(1 for day in daygenerator if day.weekday() not in [4,5])
-            self.duration= sum_minus_weekends
-            hr_public_holiday_obj = self.env['hr.public.holiday']
-            holidays_intersections_days = 0
-            for public_holiday in hr_public_holiday_obj.search(['|','&',('state', '=', 'done'),'&',('date_from', '<=',date_to),('date_to', '>=',date_to)
-                                                            ,'&',('state', '=', 'done'),'&',('date_to', '>=',date_from),('date_to', '<=',date_to)
-                                                            ]):
-                public_holiday_date_from = fields.Date.from_string(public_holiday.date_from)
-                public_holiday_date_to = fields.Date.from_string(public_holiday.date_to)
-                holidays_intersections_days +=  self.compute_intersion_days(date_from ,date_to, public_holiday_date_from, public_holiday_date_to)
-        self.duration -= holidays_intersections_days
-    
-    def compute_intersion_days(self,date_from1,date_to1,date_from2, date_to2):
-        if date_from1 >= date_from2 and date_to2 >= date_to1:
-            daygenerator = (date_from1 + timedelta(x + 1) for x in xrange((date_to1 - date_from1).days))
-            duration = sum(1 for day in daygenerator if day.weekday() not in [4,5])
-        if date_from2 >= date_from1 and date_to2 <= date_to1:
-            daygenerator = (date_from2 + timedelta(x + 1) for x in xrange((date_to2 - date_from2).days))
-            duration = sum(1 for day in daygenerator if day.weekday() not in [4,5])
-        if date_from2 >= date_from1 and date_to2 >= date_to1:
-            duration = (date_to1 - date_from2).days
-            daygenerator = (date_to1 + timedelta(x + 1) for x in xrange((date_to1 - date_from2).days))
-            duration = sum(1 for day in daygenerator if day.weekday() not in [4,5])            
-        return duration
-
-
+            self.duration = (date_to - date_from).days + 1
+            
     @api.onchange('employee_id')
     def _onchange_employee_id(self):
         if self.employee_id :
