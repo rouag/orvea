38859f29519555a197fb6f72a35fc7553bed5de5
samar <samar.sliti@smart-etech.com>
2017-02-20 15:20:50 +0100

    Add nationality and fix scholarship

--- smart_hr/models/modification.py
+++ smart_hr/models/modification.py
@@ -2,28 +2,34 @@
 
 
 from openerp import models, fields, api, _
-from openerp.exceptions import Warning
-from dateutil.relativedelta import relativedelta
-from openerp.exceptions import ValidationError
-from datetime import date, datetime, timedelta
-from openerp.tools import DEFAULT_SERVER_DATE_FORMAT, DEFAULT_SERVER_DATETIME_FORMAT
 
 
 class Contry(models.Model):
     _inherit = 'res.country'
     _description = u'البلدان'
-   
-    code=fields.Text(string='الرمز', )
+
+    code = fields.Text(string='الرمز')
     code_nat = fields.Text(string='الرمز', )
-    
     national = fields.Char(string='الجنسية', )
-   
+
+    @api.multi
+    def name_get(self):
+        if u'compute_name' in self._context:
+            return getattr(self, self._context[u'compute_name'])()
+        else:
+            return super(Contry, self).name_get()
+
+    def _get_natinality(self):
+        res = []
+        for record in self:
+            if record.national:
+                res.append((record.id, record.national))
+        return res
+
+
 class Region(models.Model):
-     _inherit = 'city.side'
-     _description = u'الجهات'
-     contry = fields.Many2one('res.country', string='البلاد')
-    
 
+    _inherit = 'city.side'
+    _description = u'الجهات'
 
-    
-   
+    contry = fields.Many2one('res.country', string='البلاد')
