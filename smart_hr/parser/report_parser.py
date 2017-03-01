29dcc9dc76c7c9456655621f4ce14a34ec9b5250
smart <smart@smart-HP-280-G1-MT>
2017-03-01 12:27:01 +0100

    [fix] card report

--- smart_hr/parser/report_parser.py
+++ smart_hr/parser/report_parser.py
@@ -207,6 +207,32 @@ class ReportHrEmployeeSituationOrderReportReport(osv.AbstractModel):
     _inherit = 'report.abstract_report'
     _template = 'smart_hr.report_hr_employee_situation_order'
     _wrapped_report_class = HrEmployeeSituationOrderReport 
+
+
+class HrEmployeeCardReport(report_sxw.rml_parse):
+
+    def __init__(self, cr, uid, name, context):
+        super(HrEmployeeCardReport, self).__init__(cr, uid, name, context=context)
+        self.localcontext.update({
+            'get_hijri_date': self._get_hijri_date,
+        })
+
+    def _get_hijri_date(self, date, separator):
+        '''
+        convert georging date to hijri date
+        :return hijri date as a string value
+        '''
+        if date:
+            date = fields.Date.from_string(date)
+            hijri_date = HijriDate(date.year, date.month, date.day, gr=True)
+            return str(int(hijri_date.year)) + separator + str(int(hijri_date.month)) + separator + str(int(hijri_date.day))
+        return None
+    
+class ReportHrEmployeeCard(osv.AbstractModel):
+    _name = 'report.smart_hr.hr_employee_card_report'
+    _inherit = 'report.abstract_report'
+    _template = 'smart_hr.hr_employee_card_report'
+    _wrapped_report_class = HrEmployeeCardReport 
     
     
     
