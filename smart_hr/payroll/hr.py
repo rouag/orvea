a0c1f7a213d18085feb19c1716be6aeede2a963d
Borni DHIFI <dhifi.borni@gmail.com>
2017-03-13 17:11:16 +0100

    [Fix]   fix function search the right salary grid

--- smart_hr/payroll/hr.py
+++ smart_hr/payroll/hr.py
@@ -13,18 +13,18 @@ class HrEmployee(models.Model):
     @api.model
     def get_salary_grid_id(self, operation_date):
         # search for  the newest salary grid detail
-        domain1 = [('grid_id.state', '=', 'done'),
-                   ('grid_id.enabled', '=', True),
-                   ('type_id', '=', self.type_id.id),
-                   ('grade_id', '=', self.grade_id.id),
-                   ('degree_id', '=', self.degree_id.id)
-                   ]
-        domain2 = domain1
+        domain = [('grid_id.state', '=', 'done'),
+                  ('grid_id.enabled', '=', True),
+                  ('type_id', '=', self.type_id.id),
+                  ('grade_id', '=', self.grade_id.id),
+                  ('degree_id', '=', self.degree_id.id)
+                  ]
         if operation_date:
             # search the right salary grid detail for the given operation_date
-            domain1.append(('date', '<=', operation_date))
-        salary_grid_id = self.env['salary.grid.detail'].search(domain1, order='date desc', limit=1)
+            domain.append(('date', '<=', operation_date))
+        salary_grid_id = self.env['salary.grid.detail'].search(domain, order='date desc', limit=1)
         if not salary_grid_id:
             # doamin for  the newest salary grid detail
-            salary_grid_id = self.env['salary.grid.detail'].search(domain2, order='date desc', limit=1)
+            domain.pop(5)
+            salary_grid_id = self.env['salary.grid.detail'].search(domain, order='date desc', limit=1)
         return salary_grid_id
