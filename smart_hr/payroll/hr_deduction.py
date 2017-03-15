b1992d9cd7437c24db33bb52ffd8e10bbd393007
abderrahmen khalledi <abderrahmen.khalledi@smart-etech.com>
2017-03-15 12:58:30 +0100

    [FIX] rename alert message in الحسميات

--- smart_hr/payroll/hr_deduction.py
+++ smart_hr/payroll/hr_deduction.py
@@ -118,7 +118,7 @@ class HrDeduction(models.Model):
     def unlink(self):
         self.ensure_one()
         if self.state != 'new':
-            raise ValidationError(u"لا يكن حذف الفروقات في حالتها الحالية ")
+            raise ValidationError(u"لا يكن حذف الحسميات في حالتها الحالية ")
         return super(HrDeduction, self).unlink()
 
 
