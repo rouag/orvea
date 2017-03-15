29a6fe89ccc66e9929fa7559365af27ba69853f5
samar <samar.sliti@smart-etech.com>
2017-03-15 18:34:36 +0100

    [fix] مراجعة  طباعة  القرار في كف اليد

--- smart_hr/parser/report_parser.py
+++ smart_hr/parser/report_parser.py
@@ -260,3 +260,30 @@ class ReportHrCard(osv.AbstractModel):
     _template = 'smart_hr.hr_emp_card_report'
     _wrapped_report_class = HrCardReport 
 
+
+class HrSuspension(report_sxw.rml_parse):
+
+    def __init__(self, cr, uid, name, context):
+        super(HrSuspension, self).__init__(cr, uid, name, context=context)
+        self.localcontext.update({
+            'get_hijri_date': self._get_hijri_date,
+        })
+
+    def _get_hijri_date(self, date, separator):
+        '''
+        convert georging date to hijri date
+        :return hijri date as a string value
+        '''
+        if date:
+            date = fields.Date.from_string(date)
+            hijri_date = HijriDate(date.year, date.month, date.day, gr=True)
+            return str(int(hijri_date.year)) + separator + str(int(hijri_date.month)) + separator + str(int(hijri_date.day))
+        return None
+
+
+class ReportHrSuspension(osv.AbstractModel):
+    _name = 'report.smart_hr.hr_suspension_report'
+    _inherit = 'report.abstract_report'
+    _template = 'smart_hr.hr_suspension_report'
+    _wrapped_report_class = HrSuspension
+    
