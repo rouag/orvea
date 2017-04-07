1134013f957bdacbab938a6caf6cda5a99b94697
samar <samar.sliti@smart-etech.com>
2017-04-07 19:01:27 +0100

    [fix] bug job number required

--- smart_hr/job/hr_job.py
+++ smart_hr/job/hr_job.py
@@ -1288,7 +1288,7 @@ class HrJobMoveGradeLine(models.Model):
 
     job_move_grade_id = fields.Many2one('hr.job.move.grade', string='الوظيفة', required=1, ondelete="cascade")
     job_id = fields.Many2one('hr.job', string='الوظيفة', required=1)
-    job_number = fields.Char(string='رقم الوظيفة', required=1, readonly=1)
+    job_number = fields.Char(string='رقم الوظيفة', readonly=1)
     job_name_code = fields.Char(related="job_id.name.number", string='الرمز', readonly=1)
     type_id = fields.Many2one('salary.grid.type', string='نوع السلم', readonly=1, required=1)
     grade_id = fields.Many2one('salary.grid.grade', string='المرتبة الحالية', readonly=1, required=1)
