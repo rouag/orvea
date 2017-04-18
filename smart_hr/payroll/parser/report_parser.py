53f1ebf14c281cb601d36f3fc0c96bd400571b9a
abderrahmen khalledi <abderrahmen.khalledi@smart-etech.com>
2017-04-18 10:12:29 +0100

    [IMP] تقرير لحصر الموظفين الذين طرأ تغيير في مسيرهم

--- smart_hr/payroll/parser/report_parser.py
+++ smart_hr/payroll/parser/report_parser.py
@@ -149,15 +149,17 @@ class ReportPayslipChangement(report_sxw.rml_parse):
             'get_lines': self._get_lines,
         })
 
-    def _get_lines(self, employee_ids, month):
+    def _get_lines(self, slip_ids, month):
         res = []
         date_from = month.date_start
         date_to = month.date_stop
         payslip_obj = self.pool.get('hr.payslip')
+        employee_ids = [rec.employee_id.id for rec in slip_ids]
+        print "employee_ids", employee_ids
         payslip__ids = payslip_obj.search(self.cr, self.uid, [('date_from', '=', date_from),
                                                               ('date_to', '=', date_to),
                                                               ('state', '=', 'done'),
-                                                              ('employee_id', 'in', employee_ids.ids)])
+                                                              ('employee_id', 'in', employee_ids)])
         for rec in payslip_obj.browse(self.cr, self.uid, payslip__ids):
             if (rec.salary_net - rec.employee_id.net_salary) != 0:
                 line_ids = rec.line_ids.search([('category', 'in', ['changing_allowance', 'difference', 'deduction'])])
