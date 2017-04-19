1cdc9a2d2879ef7338fb4fe14f8cf494e21dc30d
abderrahmen khalledi <abderrahmen.khalledi@smart-etech.com>
2017-04-19 18:12:56 +0100

    [FIX] compute_duration_diifrence

--- smart_hr/payroll/hr_payroll_inherit.py
+++ smart_hr/payroll/hr_payroll_inherit.py
@@ -247,7 +247,7 @@ class HrPayslip(models.Model):
             if date_from >= assign_date_from and assign_date_to > date_to:
                 res = self.env['hr.smart.utils'].compute_duration_difference(assign_id.employee_id, date_from, date_to, True, True, True)
             if date_from >= assign_date_from and assign_date_to <= date_to:
-                res = self.env['hr.smart.utils'].compute_duration_difference(assign_id.employee_id, assign_date_from, assign_date_to, True, True, True)
+                res = self.env['hr.smart.utils'].compute_duration_difference(assign_id.employee_id, date_from, assign_date_to, True, True, True)
             if assign_date_from >= date_from and assign_date_to < date_to:
                 res = self.env['hr.smart.utils'].compute_duration_difference(assign_id.employee_id, assign_date_from, assign_date_to, True, True, True)
             if assign_date_from >= date_from and assign_date_to >= date_to:
@@ -329,7 +329,7 @@ class HrPayslip(models.Model):
             if date_from >= scholarship_date_from and scholarship_date_to > date_to:
                 res = self.env['hr.smart.utils'].compute_duration_difference(scholarship_id.employee_id, date_from, date_to, True, True, True)
             if date_from >= scholarship_date_from and scholarship_date_to <= date_to:
-                res = self.env['hr.smart.utils'].compute_duration_difference(scholarship_id.employee_id, scholarship_date_from, scholarship_date_to, True, True, True)
+                res = self.env['hr.smart.utils'].compute_duration_difference(scholarship_id.employee_id, date_from, scholarship_date_to, True, True, True)
             if scholarship_date_from >= date_from and scholarship_date_to < date_to:
                 res = self.env['hr.smart.utils'].compute_duration_difference(scholarship_id.employee_id, scholarship_date_from, scholarship_date_to, True, True, True)
             if scholarship_date_from >= date_from and scholarship_date_to >= date_to:
@@ -420,7 +420,7 @@ class HrPayslip(models.Model):
             if date_from >= lend_date_from and lend_date_to > date_to:
                 res = self.env['hr.smart.utils'].compute_duration_difference(lend_id.employee_id, date_from, date_to, True, True, True)
             if date_from >= lend_date_from and lend_date_to <= date_to:
-                res = self.env['hr.smart.utils'].compute_duration_difference(lend_id.employee_id, lend_date_from, lend_date_to, True, True, True)
+                res = self.env['hr.smart.utils'].compute_duration_difference(lend_id.employee_id, date_from, lend_date_to, True, True, True)
             if lend_date_from >= date_from and lend_date_to < date_to:
                 res = self.env['hr.smart.utils'].compute_duration_difference(lend_id.employee_id, lend_date_from, lend_date_to, True, True, True)
             if lend_date_from >= date_from and lend_date_to >= date_to:
@@ -530,7 +530,7 @@ class HrPayslip(models.Model):
             if date_from >= holiday_date_from and holiday_date_to > date_to:
                 res = self.env['hr.smart.utils'].compute_duration_difference(holiday_id.employee_id, date_from, date_to, True, True, True)
             if date_from >= holiday_date_from and holiday_date_to <= date_to:
-                res = self.env['hr.smart.utils'].compute_duration_difference(holiday_id.employee_id, holiday_date_from, holiday_date_to, True, True, True)
+                res = self.env['hr.smart.utils'].compute_duration_difference(holiday_id.employee_id, date_from, holiday_date_to, True, True, True)
             if holiday_date_from >= date_from and holiday_date_to < date_to:
                 res = self.env['hr.smart.utils'].compute_duration_difference(holiday_id.employee_id, holiday_date_from, holiday_date_to, True, True, True)
             if holiday_date_from >= date_from and holiday_date_to >= date_to:
@@ -886,7 +886,7 @@ class HrPayslip(models.Model):
             if date_from >= suspension_date_from and suspension_date_to > date_to:
                 res = self.env['hr.smart.utils'].compute_duration_difference(employee, date_from, date_to, True, True, True)
             if date_from >= suspension_date_from and suspension_date_to <= date_to:
-                res = self.env['hr.smart.utils'].compute_duration_difference(employee, suspension_date_from, suspension_date_to, True, True, True)
+                res = self.env['hr.smart.utils'].compute_duration_difference(employee, date_from, suspension_date_to, True, True, True)
             if suspension_date_from >= date_from and suspension_date_to < date_to:
                 res = self.env['hr.smart.utils'].compute_duration_difference(employee, suspension_date_from, suspension_date_to, True, True, True)
             if suspension_date_from >= date_from and suspension_date_to >= date_to:
