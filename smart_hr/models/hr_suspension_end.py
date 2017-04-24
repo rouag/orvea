3ef454327fe0772db93ee347d8587cd263b1e64a
karim Rouag <karimrouag86@gmail.com>
2017-04-24 18:29:13 +0100

    [fix] hr suspension

--- smart_hr/models/hr_suspension_end.py
+++ smart_hr/models/hr_suspension_end.py
@@ -87,9 +87,7 @@ class hr_suspension_end(models.Model):
                 decision_type_id = self.env.ref('smart_hr.data_decision_type27').id
             if self.suspension_id.id and self.employee_id.type_id.id == self.env.ref('smart_hr.data_salary_grid_type7').id:
                 decision_type_id = self.env.ref('smart_hr.data_decision_type30').id
-            if self.suspension_id.id :
-                print'rrrrrrrrrrrrrrrrrr'
-                decision_type_id = self.env.ref('smart_hr.data_decision_type28').id
+         
             # create decission
             decission_val={
                 #'name': self.name,
